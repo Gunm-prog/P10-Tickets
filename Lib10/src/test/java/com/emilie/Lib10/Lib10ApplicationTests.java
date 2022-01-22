1f92b9b445841d63764b4ed790bfffbd1e227eac
Emilie Balsen <54776673+Gunm-prog@users.noreply.github.com>
2022-01-22 18:57:55 +0100

    add testng dep

--- Lib10/src/test/java/com/emilie/Lib10/Lib10ApplicationTests.java
+++ Lib10/src/test/java/com/emilie/Lib10/Lib10ApplicationTests.java
@@ -2,7 +2,7 @@ package com.emilie.Lib10;
 
 
 /*import org.junit.jupiter.api.Test;*/
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
