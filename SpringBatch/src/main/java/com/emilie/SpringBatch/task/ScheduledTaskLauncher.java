9157f80ab05cb7c8f612e62f2fadd1bb5cac28a6
root <em.balsen@laposte.net>
2022-05-12 11:27:43 +0200

    add timeout feign in application.properties

--- SpringBatch/src/main/java/com/emilie/SpringBatch/task/ScheduledTaskLauncher.java
+++ SpringBatch/src/main/java/com/emilie/SpringBatch/task/ScheduledTaskLauncher.java
@@ -43,7 +43,8 @@ public class ScheduledTaskLauncher {
      * First, get a security token with batch account,
      * then run the mail task.
      */
-    /*@Scheduled(cron="0 0 1 * * ?")*/
+  //  (cron="*/10 * * * * *") //10 seconds for presentation
+  //  (cron="0 0 1 * * ?") // for one day
     @Scheduled(cron="*/10 * * * * *")
     public void runScheduledRecoveryTask() {
         try{
@@ -71,32 +72,4 @@ public class ScheduledTaskLauncher {
         }
     }
 
-    /**
-     * Scheduled task run in loop for the demo
-     * (cron="0 0 1 * * ?") prod context : run task every day at 01:00 am
-     * <p>
-     * First, get a security token with batch account,
-     * then run the expired reservation task.
-     */
-    /*@Scheduled(cron="0 0 1 * * ?")*/
-
-   /* public void runScheduledExpiredReservationTask() {
-        try{
-            String accessToken=loginService.authenticateBatch();
-
-            List<Reservation> expiredResaList = reservationServices.getExpiredReservation(accessToken);
-
-            if(expiredResaList.isEmpty()){
-                log.info( "no expired reservation exist" );
-            }
-
-            for(Reservation expiredReservation : expiredResaList){
-                reservationServices.deleteExpiredReservation(accessToken, expiredReservation);
-                log.info( "expired reservation with id " + expiredReservation.getId() + " deleted" );
-            }
-
-        }catch(FeignException e){
-            log.warn( e.getMessage(), e );
-        }
-    }*/
 }
